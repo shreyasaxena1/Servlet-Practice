acb6b131233e0632b42e6cd4c6c2b04cf5c0288e
Shreya Saxena <shreya.saxena1@ihsmarkit.com>
2022-04-05 15:41:37 +0530

    changes

--- src/main/java/pservlets/AddServlet.java
+++ src/main/java/pservlets/AddServlet.java
@@ -1,20 +1,30 @@
 package pservlets;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 public class AddServlet extends HttpServlet{
     /*method belong to servlet lifecycle
       two things - request and response
       so use req object and res object
      */
-	public void service( HttpServletRequest req, HttpServletResponse res){
+	//service - mand name
+	public void service( HttpServletRequest req, HttpServletResponse res) throws IOException{
 		//getParameter return string so converting it into integer
+		
 		int a = Integer.parseInt(req.getParameter("num1"));
 		int b = Integer.parseInt(req.getParameter("num2"));	
 		
 		int ans = a+b;
+		
+		//write data on res object
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println(ans);
 		
 		
 	}
